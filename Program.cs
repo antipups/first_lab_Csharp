27acc41aa82f8ec5e1a407d9ffe4f0911d1e3698
antipups <nikitakurkurin@protonmail.ch>
2020-02-24 19:36:14 +0300

    Создание меню.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace first_lab_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            menu();
+        }
+
+        static void menu()    // меню
+        {
+            while (true)
+            {
+                switch ((int)Console.ReadKey().KeyChar)        // считываем что ввел пользователь
+                {
+                    case 49:
+                        Console.WriteLine("sex");
+                        break;
+                    case 50:
+                        Console.WriteLine("sex");
+                        break;
+                    case 51:
+                        Console.WriteLine("sex");
+                        break;
+                    case 27:
+                        return;
+                }   
+            }
+        }
+    }
+}
